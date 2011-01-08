f5ddb055b8ba24cf02e4fc13a6df784be8728530
Samuel <samuelololol@gmail.com>
2011-01-09 02:27:49 +0800

    first example done

--- chap01/original/Inventory.cpp
+++ chap01/original/Inventory.cpp
@@ -6,67 +6,61 @@ void Inventory:: addGuitar(string serialNumber, double price,
         string builder, string model, 
         string type, string backWood, string topWood)
 {
-    Guitar* guitar = new Guitar (serialNumber, price, 
+    Guitar guitar = Guitar (serialNumber, price, 
             builder, model, type,
             backWood, topWood);
 
     guitars.push_back(guitar);
 }
 
-const Guitar* Inventory::getGuitar(const string& serialNumber) const {
-    for ( list<Guitar *>::const_iterator i = guitars.begin(); i != guitars.end(); i++)
+const Guitar Inventory::getGuitar(const string& serialNumber) const {
+    for ( list<Guitar>::const_iterator i = guitars.begin(); i != guitars.end(); i++)
     {
-        if ((*i)->getSerialNumber() == serialNumber)
-            return &**i;
+        if (i->getSerialNumber() == serialNumber)
+            return *i;
     }
-    return 0;
+    throw InventoryException("Null");
 }
 
-const Guitar* Inventory::search(const Guitar& searchGuitar) const{
-    for ( list<Guitar *>::const_iterator i = guitars.begin(); i != guitars.end(); i++)
+const Guitar Inventory::search(const Guitar& searchGuitar) const{
+    for ( list<Guitar >::const_iterator i = guitars.begin(); i != guitars.end(); i++)
     {
-        // no guitar
-        if ((*i) == 0)
-            continue;
-
         // Ignore serial number since that's unique
         // Ignore price since that's unique
 
         // builder
         string builder = searchGuitar.getBuilder();
         if (!(builder.compare("")) &&
-               (!builder.compare((*i)->getBuilder())))
+               (!builder.compare(i->getBuilder())))
             continue;
         
         // model
         string model = searchGuitar.getModel();
         if (!(model.compare("")) &&
-               (!model.compare((*i)->getModel())))
+               (!model.compare(i->getModel())))
             continue;
 
         // type
         string type = searchGuitar.getType();
         if (!(type.compare("")) &&
-               (!type.compare((*i)->getType())))
+               (!type.compare(i->getType())))
             continue;
 
         // backWood
         string backWood = searchGuitar.getBackWood();
         if (!(backWood.compare("")) &&
-               (!backWood.compare((*i)->getBackWood())))
+               (!backWood.compare(i->getBackWood())))
             continue;
 
         // topWood
         string topWood = searchGuitar.getTopWood();
         if (!(topWood.compare("")) &&
-               (!topWood.compare((*i)->getTopWood())))
+               (!topWood.compare(i->getTopWood())))
             continue;
 
-        return &**i;
+        return *i;
     }
-    return 0;
+    throw InventoryException("Null");
 }
 
-Inventory::~Inventory(){
-
-}
+Inventory::~Inventory(){}
