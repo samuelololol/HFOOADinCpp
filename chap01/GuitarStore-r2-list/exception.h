b2439b937b0d7ebdf2c5ed7c6ef8d51b3a856e82
samuelololol <samuelololol@gmail.com>
2012-01-01 00:42:45 +0800

    remove #pragma once

--- chap01/GuitarStore-r2-list/exception.h
+++ chap01/GuitarStore-r2-list/exception.h
@@ -1,9 +1,7 @@
-#pragma once
 #ifndef __EXCEPTION_H__
 #define __EXCEPTION_H__
 
 class myexception : public std::exception {
-
 public:
     myexception(){}  
     myexception(const char *message) :
@@ -12,11 +10,8 @@ public:
     virtual const char * message(){
         return message_;
     }
-    
-
 protected:
     /* data */
     const char *message_;
-
 };
 #endif
